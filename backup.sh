fb7dad8800f3e804e1613b33867020cbdb2eda72
mohammadrezaahmadzadegan <mohammadrezaahmadzadegan@gmail.com>
2025-02-17 12:44:26 -0800

    start project with symfony

---
+++ backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash  
+
+# بکاپ گرفتن از داده‌های MySQL  
+tar -czvf mysql_data_backup.tar.gz ./mysql_data  
+
+# بکاپ گرفتن از فایل‌های وب  
+tar -czvf web_files_backup.tar.gz ./
---
+++ restore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash  
+
+# بازیابی داده‌های MySQL  
+tar -xzvf mysql_data_backup.tar.gz  
+
+# بازیابی فایل‌های وب  
+tar -xzvf web_files_backup.tar.gz
